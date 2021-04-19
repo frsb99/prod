4bdbd7bd58ca18a0564b23a86039e4e961951e95
Felipe R. Barbosa <72769404+frsb99@users.noreply.github.com>
2021-04-19 13:57:59 -0300

    Add files via upload

---
+++ Senior_V2.ps1
@@ -0,0 +1,24 @@
+﻿#CSV deve conter primeiro campo com computer e os demais um host por linha
+Start-Transcript -path '\\domhcor.local\comum$\inventario$\Atualiza_Senior\Atualizados.txt'
+
+
+#$outFileName="C:\Temp\PC_RONDA-done.TXT"
+$inFileName="\\filesv12\serviceDesk\Senior_Atualiza\PC_RONDA.csv"
+$computador=Import-Csv $inFileName
+$DeploySenior='\\filesv12\serviceDesk\Senior_Atualiza\Atalhos\*.lnk'
+$DeploySeniorCTRL='\\filesv12\serviceDesk\Senior_Atualiza\inst.ctrl'
+
+ForEach ($_ in $computador.computer){
+
+#New-Item -Force -Path '\\$_\C$\Users\Public\Desktop\Senior\Vetorh' -ItemType Directory 
+New-Item -Force -Path "\\$_\C$\Users\Public\Desktop\Senior\Vetorh" -ItemType Directory
+
+
+#Copy-Item "$DeploySenior" -Destination \\$_\C$\Users\public\Desktop\Senior\Vetorh\
+Copy-Item $DeploySenior -Destination "\\$_\C$\Users\public\Desktop\Senior\Vetorh" -Recurse -Force -Verbose
+Copy-Item $DeploySeniorCTRL "\\$_\C$\Senior"  -Force -Verbose
+
+#$pc | Export-Csv $outFileName
+# dir \\SIST27839\c$\temp\teste2021
+}
+Stop-Transcript
